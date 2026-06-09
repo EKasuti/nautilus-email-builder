63297c27516bfb3843146c2e7c1a4b6a422bc525
Emmanuel Kasuti Makau <emmanuel.k.makau.jr.26@dartmouth.edu>
2026-06-09 18:44:08 -0400

    feat: tabs aand local storage persistence

--- frontend/src/app/email/layout.tsx
+++ frontend/src/app/email/layout.tsx
@@ -1,14 +1,10 @@
 import { Sidebar } from "@/components/Sidebar";
-import { TopBar } from "@/components/TopBar";
 
 export default function EmailLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen">
       <Sidebar />
-      <div className="flex-1 min-w-0 flex flex-col">
-        <TopBar parent="Email Broadcast" page="Templates" />
-        {children}
-      </div>
+      <div className="flex-1 min-w-0 flex flex-col">{children}</div>
     </div>
   );
 }
